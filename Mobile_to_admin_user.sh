93421febcbb9873af8cc0755edbae1c56ba64618
hccscdocs <jhumphrey@ASC-JHumphrey-iMac.local>
2017-03-28 13:35:34 -0400

    Mobile User To Admin

---
+++ Mobile_to_admin_user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+\# generate local user account based on UID of greater than 1000, if multiple AD accounts exist this will grab all of them
+\# Script coutesy of JAMF Nation and Tom Larkin
+
+userList=$(/usr/bin/dscl . list /Users UniqueID | /usr/bin/awk '$2 > 1000 \{ print $1 }')
+
+for u in ${userList} ; do
+
+/usr/sbin/dseditgroup -o edit -a ${u} -t user admin
+
+done
+
+exit 0
