2577c02e435e9d0bd11760947177b821fa6bf008
HCCSC Mac Support <jhumphrey@hccsc.k12.in.us>
2017-05-18 11:22:04 -0400

    FW Mac Kiosk Fix

---
+++ fix_fwcld_plist_read.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#	Name: fix_fwcld_plist_read.sh
+#	Author: J Humphrey
+#	Date: May 18th, 2017
+#
+#	Notes:
+#	This is a script to fix FW Kiosk display details
+#
+#	Version Info:
+#	1.0 - Initial script	
+#	1.1 - Added Ryan Log funtions
+#
+#	ToDo:
+#
+#
+#
+
+
+
+#log all to filewave client log
+
+exec 1>>/var/log/fwcld.log
+
+exec 2>>/var/log/fwcld.log
+
+echo "Running fwcld.plist details fix script
+
+chmod o+r /usr/local/etc/fwcld.plist
+
+ 
